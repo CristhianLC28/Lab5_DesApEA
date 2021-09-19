83bd54ff58846a8e5d35d7d9fe4b2826fa50ae99
cristhian Lopez <cristhian.lopez@tecsup.edu.pe>
2021-09-19 16:34:50 -0500

    first commit

---
+++ Business/BDetallePedido.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Entity;
+
+namespace Business
+{
+    public class BDetallePedido
+    {
+        private DDetallePedido DDetallePedido = null;
+        public List<Detalle_de_Pedido> GetDetallePedidosPorId(int IdPedido)
+        {
+            List<Detalle_de_Pedido> detalle_De_Pedidos = null;
+            try
+            {
+                DDetallePedido = new DDetallePedido();
+                detalle_De_Pedidos = DDetallePedido.GetDetallePedidos(new Detalle_de_Pedido { Pedido = new Pedido { IdPedido = IdPedido } });
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DDetallePedido = null;
+            }
+            return detalle_De_Pedidos;
+        }
+
+        public decimal GetDetalleTotalPorId(int IdPedido)
+        {
+            List<Detalle_de_Pedido> detalle_De_Pedidos = null;
+            decimal total = 0;
+            try
+            {
+                DDetallePedido = new DDetallePedido();
+                detalle_De_Pedidos = DDetallePedido.GetDetallePedidos(new Detalle_de_Pedido { Pedido = new Pedido { IdPedido = IdPedido } });
+           
+                foreach(var item in detalle_De_Pedidos)
+                {
+                    total = total + item.Cantidad * item.PrecioUnidad - item.Descuento;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                DDetallePedido = null;
+            }
+            return total;
+        }
+    }
+}
---
+++ Data/DDetallePedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+
+namespace Data
+{
+    public class DDetallePedido
+    {
+        public List<Detalle_de_Pedido> GetDetallePedidos(Detalle_de_Pedido detallePedido)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return null;
+        }
+    }
+}
